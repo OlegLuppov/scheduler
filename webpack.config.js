2a455ccff8dae4ba0467f65bf1a380e0511d7b12
Oleg Luppov <lupperpuper@gmail.com>
2022-12-17 13:21:33 +0400

    feat:webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+const config = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /node_modules/,
+        loader: 'esbuild-loader',
+      },
+
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+      {
+        test: /\.mp3$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+}
+
+module.exports = config
